e6dc06a00344b1a42f92fa499dcbf9355e158543
javirojas988 <jrojagu988@g.educaand.es>
2025-12-10 10:22:05 +0100

    relo

--- tema7/RecuBucles/EjRelojGpt.java
+++ tema7/RecuBucles/EjRelojGpt.java
@@ -1,5 +1,48 @@
 package RecuBucles;
-
+import java.util.Scanner;
 public class EjRelojGpt {
+    public static void Superior(boolean respuesta0 ){ ; System.out.print(( respuesta0 == true)?" *** " : "     "); }
+    public static void SupIZq(boolean respuesta1 ) { ; System.out.print(( respuesta1 == true)?"*   " : "    "); }
+    public static void SupDer(boolean respuesta2) { ; System.out.print(( respuesta2 == true)?"*" : " "); }
+    public static void Centro(boolean respuesta3 ) { ; System.out.print(( respuesta3 == true)?" *** " : "     "); }
+    public static void InfDer(boolean respuesta4 ) { ; System.out.print(( respuesta4 == true)?"*" : " "); }
+    public static void InfIzq(boolean respuesta5 ) { ; System.out.print(( respuesta5 == true)?"*   " : "    "); }
+    public static void Inferior(boolean respuesta6 ) { ; System.out.print(( respuesta6 == true)?" *** " : "     "); }
     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int digito = 5;
+
+        for ( int i = 0 ; i <= 7 ; i++) {
+            switch (digito) {
+                case 1:
+
+                    break; 
+                case 2 : 
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 0:
+                    break;
+            }
+            System.out.println();
+        }
+        
+
+        
+        
+        
+    }
 }
