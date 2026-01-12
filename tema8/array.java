ae66ca139c10a89a4b9ec6d01d0690d739638778
javirojas988 <jrojagu988@g.educaand.es>
2026-01-12 11:08:50 +0100

    COMIT PAPUY ARRIVE

---
+++ tema8/array.java
@@ -0,0 +1,32 @@
+package tema8;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // float [] votos = new float[4] ;
+        // votos[0] = 1000000;
+
+
+        int respuesta ; 
+        float[] votos = { 1 , 2 , 67 , 56 ,54};
+        try {
+            respuesta = sc.nextInt();
+            System.out.println("introduze num 1-5");
+            System.out.println(votos[respuesta - 1]);
+
+            
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Introduze un valor del 1-5");
+        } catch (InputMismatchException e ){
+            System.out.println("Introduze un número");
+        }
+
+
+
+        //cerramo flujo de la entrada
+        sc.close() ; 
+    }
+}
---
+++ tema8/ejerP1.java
@@ -0,0 +1,7 @@
+package tema8;
+import java.util.Scanner;
+public class ejerP1 {
+    //lee 10 número por teclao y luego printeamos en orden invero
+    Scanner sc = new Scanner(System.in);
+    for(int i)
+}
