57b2cff9a1d018bc472fd02601c0acf449857cdc
javirojas988 <jrojagu988@g.educaand.es>
2026-01-16 12:55:26 +0100

    zs

---
+++ tema8/eje8.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class eje8 {
+    public static void main(String[] args) {
+        int[] temperaturaMedia = new int[12];
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Introduzca la temperatura de Enero:");
+        temperaturaMedia[0] = sc.nextInt() ; 
+        System.out.println("Introduzca la temperatura de Feb:");
+        temperaturaMedia[1] = sc.nextInt() ; 
+        System.out.println("Introduzca la temperatura de Mar:");
+        temperaturaMedia[2] = sc.nextInt() ; 
+        System.out.println("Introduzca la temperatura de Abril:");
+        temperaturaMedia[3] = sc.nextInt() ; 
+        System.out.println("Introduzca la temperatura de Mayo:");
+        temperaturaMedia[4] = sc.nextInt() ; 
+        System.out.println("Introduzca la temperatura de julio:");
+        temperaturaMedia[5] = sc.nextInt() ; 
+        System.out.println("Introduzca la temperatura de jun:");
+        temperaturaMedia[6] = sc.nextInt() ; 
+        System.out.println("Introduzca la temperatura de agosto:");
+        temperaturaMedia[7] = sc.nextInt() ; 
+        System.out.println("Introduzca la temperatura de Sep:");
+        temperaturaMedia[8] = sc.nextInt() ; 
+        System.out.println("Introduzca la temperatura de Octu:");
+        temperaturaMedia[9] = sc.nextInt() ; 
+        System.out.println("Introduzca la temperatura de Nov:");
+        temperaturaMedia[10] = sc.nextInt() ; 
+        System.out.println("Introduzca la temperatura de Diciembre: ");
+        temperaturaMedia[11] = sc.nextInt() ; 
+
+        // Se hace array string meses 
+    }
+}
