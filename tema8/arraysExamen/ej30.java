5eefefc405f02bf79d59166b61280ff4f6669d38
javirojas988 <jrojagu988@g.educaand.es>
2026-02-05 21:02:35 +0100

    @practica en ubisof

--- tema8/arraysExamen/ej30.java
+++ tema8/arraysExamen/ej30.java
@@ -6,6 +6,10 @@ import java.util.Scanner;
 public class ej30 {
     public static Scanner sc = new Scanner(System.in);
     public static final int TABLERO = 3;
+    public static void waifu(){
+        System.out.printf(" ██████   ██████\n █        █\n █  ████  █  ████\n █     █  █     █\n ██████   ██████"
+);;
+}
     public static void pinta(String[][]tablero){
         int numero=1;
         for (int y = 0; y < TABLERO; y++) {
@@ -28,6 +32,62 @@ public class ej30 {
         System.out.println();
     }
 
+    public static boolean comprobación(String[][]tablero){
+        boolean gana = false ; 
+        int JUGADOR1 = 0 ; 
+        int JUGADOR2 = 0 ;
+        int x = 0 ;
+        int y = 0 ; 
+
+        while ( y < TABLERO && JUGADOR1 != 3 && JUGADOR2 != 3 ){ //VERTICAL
+            while ( x < TABLERO && JUGADOR1 != 3 && JUGADOR2 != 3 ){
+                if ( tablero[y][x].equals(" X ")){
+                    JUGADOR1++ ; 
+                    if (JUGADOR1==3){
+                        gana = true ; 
+                    }
+                }
+                if ( tablero[y][x].equals(" 0 ")){
+                    JUGADOR2++ ; 
+                    if (JUGADOR2==3){
+                        gana = true ; 
+                    } 
+                }  
+                x++;    
+            }
+            y++;
+            x = 0 ; 
+            JUGADOR1 = 0 ; 
+            JUGADOR2 = 0 ; 
+        }
+        x=0 ; 
+        y=0 ; 
+        while ( x < TABLERO && JUGADOR1 != 3 && JUGADOR2 != 3 ){ // HORIZONTAL
+            while ( y < TABLERO && JUGADOR1 != 3 && JUGADOR2 != 3 ){
+                if ( tablero[y][x].equals(" X ")){
+                    JUGADOR1++ ; 
+                    if (JUGADOR1==3){
+                        gana = true ; 
+                    }
+                }
+                if ( tablero[y][x].equals(" 0 ")){
+                    JUGADOR2++ ; 
+                    if (JUGADOR2==3){
+                        gana = true ; 
+                    } 
+                }  
+                x++;    
+            }
+            y++;
+            x = 0 ; 
+            JUGADOR1 = 0 ; 
+            JUGADOR2 = 0 ; 
+        }
+        
+
+        return gana ;  
+    }
+
     //mencion honorifica a billie elish 
     public static void main(String[] args) {
         String[][]tablero = new String[TABLERO][TABLERO];
@@ -40,22 +100,34 @@ public class ej30 {
                 tablero[y][x]=" . ";
             }
         }
-        
+        pinta(tablero);
         while (gana == false ){
-            pinta(tablero);
-            System.out.print("INTRODUCE CORDENADA X");
+            System.out.println("JUGADOR 1 ");
+            System.out.print("INTRODUCE CORDENADA X ");
             rX = sc.nextInt();
-            System.out.print("INTRODUCE CORDENADA Y");
+            System.out.print("INTRODUCE CORDENADA Y ");
             rY = sc.nextInt(); 
-            tablero[rY-1][rX-1] = " X " ; 
+            tablero[rX-1][rY-1] = " X " ; 
             pinta(tablero);
-            System.out.println("JUGADOR 2 ");
-            System.out.print("INTRODUCE CORDENADA X");
-            rX = sc.nextInt();
-            System.out.print("INTRODUCE CORDENADA Y");
-            rY = sc.nextInt(); 
-            tablero[rY-1][rX-1] = " 0 " ; //
-            
+            if (comprobación(tablero) == true) {
+                System.out.println("GANASTE JUGADOR 1 ");
+                gana = true ; 
+                waifu();
+            } else {
+                System.out.println("JUGADOR 2 ");
+                System.out.print("INTRODUCE CORDENADA X ");
+                rX = sc.nextInt();
+                System.out.print("INTRODUCE CORDENADA Y ");
+                rY = sc.nextInt(); 
+                tablero[rX-1][rY-1] = " 0 " ; 
+                pinta(tablero);
+                if (comprobación(tablero) == true) {
+                    System.out.println("GANASTE JUGADOR 2 ");
+                    gana = true ; 
+                    waifu();
+                }
+            }
         }
+
     }
 }
