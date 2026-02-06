3a9fc0dd1544856daa48458a9d39d8aae53e3244
javirojas988 <jrojagu988@g.educaand.es>
2026-02-06 10:49:01 +0100

    adasdasd

---
+++ tema9/ej1/clases/caballo.java
@@ -0,0 +1,10 @@
+package tema9.ej1.clases;
+
+public class caballo {
+    private String nombre ; 
+    private int altura ;
+
+    Caballo(Stirng nombre){
+        this.nombre = nombre ; 
+    }
+}
---
+++ tema9/ej1/main.java
@@ -0,0 +1,7 @@
+package tema9.ej1; 
+import clases.Caballo; 
+public class main {
+    public static void main(String[] args) {
+        Caballo montañoso = new Caballo();
+    }
+}
