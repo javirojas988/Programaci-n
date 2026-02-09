fa08d06db8caad058653746b18a8191987c97701
javirojas988 <jrojagu988@g.educaand.es>
2026-02-09 21:50:12 +0100

    sssss

--- tema9/ej1/Persona.java
+++ tema9/ej1/Persona.java
@@ -2,15 +2,33 @@ public class Persona {
     private String nombre ;  // private ( encapsulamiento) 
     private String nacionalidad;
 
-    void saluda(){ // instancia
+    void saluda(){ // metodo -lo que realiza (lowerCamelCase)
         System.out.println("hola"); 
     }
 
-    Persona (String nacionalidad){ // constructor, nombr igual q la clase, para iniciar propiedades
-        this.nacionalidad=nacionalidad;  // this q hace referencia al objeto instanciado .
+    public Persona(){ // constructor vacio
+
     }
 
-    void anda(String manera){ // parametro , puede ser cualquier tipo de dato
+    Persona (String nacionalidad, String nombre){ // constructor, nombr igual q la clase, para iniciar propiedades
+        this.nacionalidad=nacionalidad;  // this q hace referencia al objeto instanciado .
+        this.nombre=nombre ; 
+    }
+
+    void anda(String manera){ // metodo puede tener parametro. (X) -> parametro , puede ser cualquier tipo de dato
         System.out.println("ando"+manera); 
+    }
+
+    public String getNombre(){ // traer
+        return nombre; 
+    }
+    public void setNombre(String nombre){ // colocar  
+        this.nombre = nombre; 
+    }
+    public String getNacionalidad(){
+        return nacionalidad;
+    }
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
     }
 }
--- tema9/ej1/main.java
+++ tema9/ej1/main.java
@@ -1,6 +1,20 @@
 // import gato no lo uso porque estan en el mismo paquete 
 
 public class main {
-    Persona javi = new Persona("Checoeslovaco");
-    javi.saluda() ; //no va
-}   
+    public static void main(String[] args) {
+        Persona alu1 = new Persona("Checoeslovaco","javi"); // con el  constructor con datos que van a tener ese objeto 
+        Persona alu2 = new Persona(); // con el constructor vacio 
+        
+        // int nombre = alu2.getNombre();
+        System.out.println("Nacionalidad alu1 "+ alu1.getNacionalidad());
+        System.out.println("Nombre alu1 "+ alu1.getNombre() );
+
+        alu2.setNacionalidad("peruano");
+        alu2.setNombre("isaac");
+        
+        System.out.println("Nombre alu2 "+ alu2.getNombre() );
+        System.out.println("nacionalidad alu2 : "+ alu2.getNacionalidad());
+        alu1.saluda();
+    }
+   
+}     
